07b189ddeeaaca2baa0c1accedd3ae5744b2cf46
blj7097 <134014668+blj7097@users.noreply.github.com>
2025-07-11 11:11:56 +0900

    8추가

--- Week3/ViewController.swift
+++ Week3/ViewController.swift
@@ -13,11 +13,13 @@ class ViewController: UIViewController {
 		super.viewDidLoad()
 		print(1)
 		print(2)
-	 	print(3)
+		print(3)
 		print(4)
 		print(5)
 		print(6)
 		print(7)
+		print(8)
+		print(9)
 	}
 }
 
